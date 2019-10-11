605b870a8ecd4087295125a5128692dcf5186b46
Ludovic Marcotte <ludovic@miraclemax.local>
2019-10-11 12:53:46 -0400

    (fix) added missing quote to avoid exception

--- custom/sogo-demo/chrome/content/sogo-connector/general/custom-preferences.js
+++ custom/sogo-demo/chrome/content/sogo-connector/general/custom-preferences.js
@@ -1,4 +1,4 @@
 force_int_pref("calendar.autorefresh.timeout", 30);
 force_int_pref("calendar.invitations.autorefresh.timeout", 30);
 force_bool_pref("sogo-connector.disable-send-invitations-checkbox", false);
-force_char_pref("sogo-connector.autoComplete.commentAttribute, "JobTitle");
+force_char_pref("sogo-connector.autoComplete.commentAttribute", "JobTitle");
