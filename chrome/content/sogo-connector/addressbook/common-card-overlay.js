1b1e965fc978968312d1c24cc98e7ad7bde62ff7
Ludovic Marcotte <lmarcotte@inverse.ca>
2020-10-09 13:37:52 -0400

    Cleanups and don't always add a category

--- chrome/content/sogo-connector/addressbook/common-card-overlay.js
+++ chrome/content/sogo-connector/addressbook/common-card-overlay.js
@@ -1,16 +1,34 @@
-/* -*- Mode: js2-mode; tab-width: 4; c-tab-always-indent: t; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* common-card-overlay.js - This file is part of "SOGo Connector".
+ *
+ * Copyright: Inverse inc., 2006-2019
+ *     Email: support@inverse.ca
+ *       URL: http://inverse.ca
+ *
+ * "SOGo Connector" is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation;
+ *
+ * "SOGo Connector" is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * "SOGo Connector"; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 function jsInclude(files, target) {
-    let loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
-                           .getService(Components.interfaces.mozIJSSubScriptLoader);
-    for (let i = 0; i < files.length; i++) {
-        try {
-            loader.loadSubScript(files[i], target);
-        }
-        catch(e) {
-            //dump("common-card-overlay.js: failed to include '" + files[i] + "'\n" + e + "\n");
-        }
+  let loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+      .getService(Components.interfaces.mozIJSSubScriptLoader);
+  for (let i = 0; i < files.length; i++) {
+    try {
+      loader.loadSubScript(files[i], target);
     }
+    catch(e) {
+      //dump("common-card-overlay.js: failed to include '" + files[i] + "'\n" + e + "\n");
+    }
+  }
 }
 
 jsInclude(["chrome://sogo-connector/content/addressbook/categories.js",
@@ -20,19 +38,8 @@ jsInclude(["chrome://sogo-connector/content/addressbook/categories.js",
            "chrome://sogo-connector/content/global/sogo-config.js"]);
 
 let gSCCardValues = {
-    documentDirty: false,
-    categories: [],
-
-    // This is necessary to allow the listener of webdavPutString and the
-    // upload Observer to remain in scope since the dialog is closed before
-    // the listener can do its job.
-
-    messengerWindow: Components.classes["@mozilla.org/appshell/window-mediator;1"]
-                               .getService(Components.interfaces.nsIWindowMediator)
-                               .getMostRecentWindow("mail:3pane"),
-    abWindow: Components.classes["@mozilla.org/appshell/window-mediator;1"]
-                        .getService(Components.interfaces.nsIWindowMediator)
-                        .getMostRecentWindow("mail:addressbook")
+  documentDirty: false,
+  categories: [],
 };
 
 var _window;
@@ -41,50 +48,50 @@ var _document;
 function SCOnCommonCardOverlayLoad(window, document) {
   _window = window;
   _document = document;
-
-  //if (typeof(SCOnCommonCardOverlayLoadPreHook) == "function") {
-  //    SCOnCommonCardOverlayLoadPreHook();
-  //}
-  SICommonCardOverlay.onLoadHook();
   
+  SICommonCardOverlay.onLoadHook();
+  let catsArray = []
     /* categories */
-    let cardCategoriesValue = _window.gEditCard.card.getProperty("Categories", "");
-    let catsArray = multiValueToArray(cardCategoriesValue);
+  let cardCategoriesValue = _window.gEditCard.card.getProperty("Categories", "");
+
+  if (cardCategoriesValue.length > 0)
+    catsArray = multiValueToArray(cardCategoriesValue);
+
+  gSCCardValues.categories = SCContactCategories.getCategoriesAsArray();
+
+  /* we first check whether all the card categories exist in the prefs */
+  let max = catsArray.length;
+  let newCategories = [];
+  for (let i = 0; i < max; i++) {
+    let catName = catsArray[i];
+    if (gSCCardValues.categories.indexOf(catName) == -1
+        && newCategories.indexOf(catName) == -1) {
+      newCategories.push(catName);
+    }
+  }
+  if (newCategories.length > 0) {
+    gSCCardValues.categories = gSCCardValues.categories.concat(newCategories);
+    SCContactCategories.setCategoriesAsArray(gSCCardValues.categories);
     gSCCardValues.categories = SCContactCategories.getCategoriesAsArray();
+  }
 
-    /* we first check whether all the card categories exist in the prefs */
-    let max = catsArray.length;
-    let newCategories = [];
-    for (let i = 0; i < max; i++) {
-        let catName = catsArray[i];
-        if (gSCCardValues.categories.indexOf(catName) == -1
-            && newCategories.indexOf(catName) == -1) {
-            newCategories.push(catName);
-        }
-    }
-    if (newCategories.length > 0) {
-        gSCCardValues.categories = gSCCardValues.categories.concat(newCategories);
-        SCContactCategories.setCategoriesAsArray(gSCCardValues.categories);
-        gSCCardValues.categories = SCContactCategories.getCategoriesAsArray();
-    }
+  /* we now add the combo boxes */
+  for (let i = 0; i < max; i++) {
+    SCAppendCategory(catsArray[i]);
+  }
+  let emptyField = document.getElementById("abEmptyCategory");
+  emptyField.addEventListener("click", SCOnEmptyFieldFocus, false);
 
-    /* we now add the combo boxes */
-    for (let i = 0; i < max; i++) {
-        SCAppendCategory(catsArray[i]);
-    }
-    let emptyField = document.getElementById("abEmptyCategory");
-    emptyField.addEventListener("click", SCOnEmptyFieldFocus, false);
-
-    /* events */
-    //let tabPanelElement = document.getElementById("abTabPanels");
-    //let menulists = tabPanelElement.getElementsByTagName("menulist");
-    //for (let i = 0; i < menulists.length; i++) {
-    //    menulists[i].addEventListener("mouseup", setDocumentDirty, true);
-    //}
-    //let textboxes = tabPanelElement.getElementsByTagName("html:input");
-    //for (let i = 0; i < textboxes.length; i++) {
-    //    textboxes[i].addEventListener("change", setDocumentDirty, true);
-    //}
+  /* events */
+  //let tabPanelElement = document.getElementById("abTabPanels");
+  //let menulists = tabPanelElement.getElementsByTagName("menulist");
+  //for (let i = 0; i < menulists.length; i++) {
+  //    menulists[i].addEventListener("mouseup", setDocumentDirty, true);
+  //}
+  //let textboxes = tabPanelElement.getElementsByTagName("html:input");
+  //for (let i = 0; i < textboxes.length; i++) {
+  //    textboxes[i].addEventListener("change", setDocumentDirty, true);
+  //}
 }
 
 function SCOnEmptyFieldFocus(event) {
@@ -95,8 +102,6 @@ function SCOnEmptyFieldFocus(event) {
 
 function SCAppendCategory(catValue) {
   let vbox = _document.getElementById("abCategories");
-  //let menuList = document.createElement("menulist", { is : "menulist-editable" });
-  //menuList.setAttribute("is", "menulist-editable");
   let menuList = _document.createXULElement("menulist");
   menuList.setAttribute("is", "menulist-editable");
   menuList.setAttribute("editable", true);
@@ -104,28 +109,16 @@ function SCAppendCategory(catValue) {
   menuList.addEventListener("change", SCOnCategoryChange, false);
   menuList.addEventListener("command", SCOnCategoryChange, false);
   SCResetCategoriesMenu(menuList, catValue);
-  //menuList.value = catValue;
-  //menuList.label = catValue;
-  
   vbox.appendChild(menuList);
 
   return menuList;
 }
 
 function SCResetCategoriesMenu(menu, catValue) {
-  //let popups = menu.getElementsByTagName("menupopup");
   let itemToSelect = null;
-
-  //for (let i = 0; i < popups.length; i++) {
-  //  menu.removeChild(popups[i]);
-  //}
   menu.removeAllItems();
   
-  //let menuPopup = document.createXULElement("menupopup");
   for (let catName of gSCCardValues.categories) {
-    //let item = document.createElement("menuitem");
-    //item.setAttribute("label", catName);
-    //menuPopup.appendChild(item);
     let item = menu.appendItem(catName);
     if (catName == catValue) {
       dump("selecting item: " + catValue + "\n");
@@ -134,12 +127,9 @@ function SCResetCategoriesMenu(menu, catValue) {
   }
 
   menu.selectedItem = itemToSelect;
-  //menu.appendChild(menuPopup);
 }
 
 function SCOnCategoryBlur() {
-  //let value = this.inputField.value
-  //                .replace(/(^[ ]+|[ ]+$)/, "", "g");
   let value = this.label.replace(/(^[ ]+|[ ]+$)/, "", "g");
 
   if (value.length == 0) {
@@ -149,7 +139,6 @@ function SCOnCategoryBlur() {
 
 function SCOnCategoryChange() {
   if (this.selectedIndex == -1) { // text field was changed
-    //let value = this.inputField.value;
     let value = this.label;
     if (value.length > 0) {
       if (gSCCardValues.categories.indexOf(value) < 0) {
@@ -171,8 +160,6 @@ function SCSaveCategories() {
   let menuLists = vbox.getElementsByTagName("menulist");
   let catsArray = [];
   for (var i = 0; i < menuLists.length; i++) {
-      //let value = menuLists[i].inputField.value
-    //                        .replace(/(^[ ]+|[ ]+$)/, "", "g");
     let value = menuLists[i].label.replace(/(^[ ]+|[ ]+$)/, "", "g");
     if (value.length > 0 && catsArray.indexOf(value) == -1) {
       catsArray.push(value);
@@ -181,78 +168,6 @@ function SCSaveCategories() {
   _window.gEditCard.card.setProperty("Categories", arrayToMultiValue(catsArray));
 }
 
-// function getUri() {
-//     let uri;
-
-//     if (window.gEditCard.abURI && gEditCard.abURI == kAllDirectoryRoot + "?") { // Find the correct address book for "All Address Books"
-//         let dirId = window.gEditCard.card.directoryId
-//                              .substring(0, window.gEditCard.card.directoryId.indexOf("&"));
-//         uri = MailServices.ab.getDirectoryFromId(dirId).URI;
-//     }
-//     else if (document.getElementById("abPopup")) {
-//         uri = document.getElementById("abPopup").value;
-//     }
-//     else if (window.arguments[0].abURI) {
-//         uri = window.arguments[0].abURI;
-//     }
-//     else
-//         uri = window.arguments[0].selectedAB;
-
-//     return uri;
-// }
-
-// function setDocumentDirty(boolValue) {
-//     gSCCardValues.documentDirty = boolValue;
-// }
-
-// function saveCard(isNewCard) {
-//     try {
-//         let parentURI = getUri();
-//         let uriParts = parentURI.split("/");
-//         parentURI = uriParts[0] + "//" + uriParts[2];
-
-//         if (gSCCardValues.documentDirty
-//             && isCardDavDirectory(parentURI)) {
-//             SCSaveCategories();
-//             //let oldDavVersion = window.gEditCard.card.getProperty("groupDavVersion", "-1");
-//             //window.gEditCard.card.setProperty("groupDavVersion", "-1");
-//             //window.gEditCard.card.setProperty("groupDavVersionPrev", oldDavVersion);
-
-//             let abManager = Components.classes["@mozilla.org/abmanager;1"]
-//                                       .getService(Components.interfaces.nsIAbManager);
-//             let ab = abManager.getDirectory(parentURI);
-//             ab.modifyCard(window.gEditCard.card);
-
-//             // We make sure we try the messenger window and if it's closed, the address book
-//             // window. It might fail if both of them are closed and we still have a composition
-//             // window open and we try to modify the card from there (from the contacts sidebar)
-//             if (gSCCardValues.messengerWindow)
-//                 gSCCardValues.messengerWindow.SCSynchronizeFromChildWindow(parentURI);
-//             else
-//                 gSCCardValues.abWindow.SCSynchronizeFromChildWindow(parentURI);
-
-//             setDocumentDirty(false);
-//         }
-//     }
-//     catch(e) {
-//         if (typeof gSCCardValues.messengerWindow.exceptionHandler != "undefined")
-//             gSCCardValues.messengerWindow.exceptionHandler(null, "saveCard", e);
-//     }
-// }
-
-// function inverseInitEventHandlers() {
-// // 	if (isGroupdavDirectory(getUri()))
-// // 		RegisterSaveListener(setGroupDavFields);
-// 	inverseSetupFieldsEventHandlers();
-// }
-
-//function isLDAPDirectory(uri) {
-//    let ab = GetDirectoryFromURI(uri);
-//
-//    return (ab.isRemote && !isCardDavDirectory(uri));
-//}
-
-// From SOGo Integrator
 let SICommonCardOverlay = {
   initialCategories: null,
 
@@ -260,23 +175,15 @@ let SICommonCardOverlay = {
     this.initialCategories = SCContactCategories.getCategoriesAsString();
 
     let this_ = this;
-    //window.addEventListener("unload",
-    //                        function () { this_.onUnload(); },
-    //                        false);
+    _window.addEventListener("unload",
+                             function () { this_.onUnload(); },
+                             false);
   },
 
   onUnload: function SICO_onUnload(event) {
     let newCategories = SCContactCategories.getCategoriesAsString();
     if (newCategories != this.initialCategories) {
       SIContactCategories.synchronizeToServer();
-        }
+    }
   }
 };
-
-//let SCOnCommonCardOverlayLoadPreHook = function() { SICommonCardOverlay.onLoadHook(); };
-
-//window.addEventListener("load", SCOnCommonCardOverlayLoad, false);
-//function onLoad(activatedWhileWindowOpen) {
-//  dump("common-card-overlay.js: onLoad()\n");
-//  SCOnCommonCardOverlayLoad();
-//}
